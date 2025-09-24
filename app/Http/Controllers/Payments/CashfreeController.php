fb35ab1fba053b5263a56b68691c6b78cf75ae88
cgupta1991 <hdworldund@gmail.com>
2025-09-24 12:14:20 +0530

    file added

---
+++ app/Http/Controllers/Payments/CashfreeController.php
@@ -0,0 +1,32 @@
+<?php //00388
+// IONCUBE ENCODER 14.0 EVALUATION
+// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
+// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+
+if(extension_loaded('ionCube Loader')){$die = 'The file '.__FILE__." is corrupted.\n";}$echo=0;$echo="\nScript error: the ".(($cli=(@php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n";$ÙØØØØ = "kzsRxe4mutyw6gvnpaf_+ob jlHTqrchid";$·ØØ§‚ª = "squaxg41t6bZ39rv2mnypf7cowlie08h5dkjz";$Ù§Ø¶ªÙ¨ = __FILE__;$ØØ­ = $ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[30]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[12]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[29]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[7]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[16]];$ÙÙØÙºÙ·¸ = $ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[12]].$ÙØØØØ[$·ØØ§‚ª[7]].$ÙØØØØ[$·ØØ§‚ª[30]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[29]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[7]].$ÙØØØØ[$·ØØ§‚ª[22]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[9]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[16]];$Ù‡Ø´¶·¯Øˆ = $ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[6]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[9]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[7]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[12]].$ÙØØØØ[$·ØØ§‚ª[32]];$´³‡®Ø…ª¬Øµ° = $ÙØØØØ[$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[6]];$³ØØ„±­ØØ³Ø³Ø = $ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[6]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[22]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[32]];$¯ØØ·ªØØØÙ¨ØØØ = $ÙØØØØ[$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[29]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[7]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[22]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[16]];$ÙØØ¨†ØØ¶ØƒØØØ­ = $ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[30]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[32]];$Ø®¯ØˆØµØØØØˆ¶Ø = $ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[22]].$ÙØØØØ[$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[9]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[12]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[29]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[7]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[12]].$ÙØØØØ[$·ØØ§‚ª[32]];$Ø„Ù¬Ø­®¸±ØÙØØØ·² = $ÙØØØØ[$·ØØ§‚ª[16]].$ÙØØØØ[$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[13]].$ÙØØØØ[$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[9]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[32]].$ÙØØØØ[$·ØØ§‚ª[7].$·ØØ§‚ª[22]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[29]].$ÙØØØØ[$·ØØ§‚ª[32]];$„¸Ù°«Ù‡‡ØØºØ¹²µƒØ = $ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[9]].$ÙØØØØ[$·ØØ§‚ª[12]].$ÙØØØØ[$·ØØ§‚ª[16].$·ØØ§‚ª[29]].$ÙØØØØ[$·ØØ§‚ª[12].$·ØØ§‚ª[29]];$ØØÙ­ØØÙ³Ø‚ = $ØØ­($Ù§Ø¶ªÙ¨);$ØŠÙØ = $Ù‡Ø´¶·¯Øˆ($„¸Ù°«Ù‡‡ØØºØ¹²µƒØ, $ØØÙ­ØØÙ³Ø‚);$¯ØØ·ªØØØÙ¨ØØØ($ØŠÙØ[0], $³ØØ„±­ØØ³Ø³Ø) ? $ÙØØ¨†ØØ¶ØƒØØØ­ : _;$Ù‡Ø´¶·¯Øˆ = $„¸Ù°«Ù‡‡ØØºØ¹²µƒØ;eval($ÙÙØÙºÙ·¸($´³‡®Ø…ª¬Øµ°($ÙÙØÙºÙ·¸($´³‡®Ø…ª¬Øµ°($Ø®¯ØˆØµØØØØˆ¶Ø($Ø„Ù¬Ø­®¸±ØÙØØØ·²("\n", "", $ØŠÙØ[1])))))));
+?>
+HR+cMbBGr3iczVfbjts2EH1fYP+BBYxQQn1JA/ShNuxFkDTIPhRdNN2nXUOgJcpmI5EqSe2uEeTf
+O7yIutlJty3aCoYtizPDmcMzh9TlxeUFJyVVFUkpel1V9++1ru7fCK6lKAoq1f0NOZaUa7W6BNta
+nbFq71et1U8io4W6/1lmpx7fFISr8eNbFYyvi6IuGSeaugl/ob/XVOnx6Ie6qoTU9+9ISjKqrLXN
+Ny2IUugNUYdcUtomieiTpjyDofDo8uLT5QWCq5LsAYKivOapZoKjHVH0VhZRvERKS8b3zs6bm0tS
+XUuOUsFzto9w5SGbp37iOVNJJUVW24A4Ruv1GmEta4rbIOa6QvgAuavlYkEq1vqnolxU+4HxsjVW
+hGc78TRyWDmPzwaLk7UdKAAmlamNSEmO50q760+NDW5Q4uzXY0UxWm8QJlVVsJSYqIvfFFQ5Hbg8
+zaCk2QPMZjCwPq9evvp+9vI7+JywTgtmZmCZtT2PLUycgFF8NoKiKVTxlSjOKPlIj71I2xGC9Q7K
+bAGEZU2pUtFEGJZP0aQCWseBTJN0h9ZIilrT3qxFsSPpRzxFd3gPC/JIjg6RJh+8jf3ME7cCQEAI
+ZMLNUQQ0TKAUTRiHmdPdFOErYBWw54VNA/BYf2ruPmPDlKtTA/GqKWsCuRWCZDBHZ6lxY2qTcyXO
+Ns3D6ciQlKLmDukoh3A6RrwudzCSC1kSHVl0ZhugYUqn6BXkPQcMcA9yHyutpaQ8dbhAwVpAk1PZ
+hFDHcieKnl9aKy1KcM0oIFMo6zkkbjDyRUWupeNQHUgLfPeq6zvSEoL3AHEu9vlZr+oguOuUStJ9
+IimUkQInFvdvv11YDKaDeNYDXcGi/hCuMcvDBEbGT2Rlvs2QDfTGG3e7bTuGvgT8ToLnqJjU0tcf
+qDnMigvN8mNrCBQdz7ht2SdhBxIcRH2NrHgvl49MH957cZroA1OzTdCquI0FMAmlG4ug1NAkeGGL
+UQZXT+6W7ixHUZh0tslh5WgGgU8on6QZkzTVUTzbjPr4QNOP8Mz2seHlte8Ux1GWbeM+MLONqetH
+KQXUkSQRfmdnRlogxplmRpt9dGQsw9Y1x3E8lKKJt/wA+gNSdG2aNyOaJHuqu9UZNY5ioJi3T5Rz
+sKq5agH5ZhTw38fjlpNdQQ0eUESAwieMcinKL0NSwiECYPgrO7GRz86QEU2/rTbbqEfggdHHE6Gh
+aNh14SClw2AA0jS4wcD8WlqaG5Orq8Hmf3p/abaKyB9+TK/Ym3i0VbvGtzRojGYb+JHHqBVy2CWW
+nWHGq1oPh7sk8naQhaWQGZpb63nrs2pzcDzyifTo82UKHUCWcOzoEGEC5zFLor67lRYdThwZ4fu+
+lgWjUu2tjaGUP8AivyvAOYeiksHC8P18pKfbbjGBVkGjANpnyZPjuIXyvEItMPwBqYTdDhgRBfRa
+uWqQtTmckav/DN+2ZeFsB6rfdq0mulbPwti5jHSso2GOdc6uxzwHG7ja1w1YoQOVtMPsaegPgCFn
+EnaNMcCeuv8PYG0hCLZSlMOxKnsOkJYsDZhG4W5eX7/Fo7Kao0KAHRsFK6imGV6dMSUPdACcHTbq
+BgHsS91ymTOeRY2LGYIViIcRzdEEXMwL38DDjHiPvo+ty/q9eOGP2sOaQmg/sT38rJstSDNd0NWX
+PQz3Wg/z77xDwUqm4cD5AAfavs8ddk8Ta4K352PAjsEeaKLqnUolq6zsw1K4x8N16Ph1HRL6VAE1
+jfZz8Wj4SbLsLTmqKKR0VMMV6MZqlrU79nkI/z/bBqpOzavTnxZw/6oFRXrPvC6K43Pa4m/m/9UW
+Ppm2aeDQVv1st50DDHz+AEhH77v6mgVlAZx4
